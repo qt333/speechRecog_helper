80af0d7fc258adeea52cc35e8ea2f20b0c13b5c1
D_K <D_K@email.com>
2023-03-19 22:35:41 +0200

    rdp todolist mouseapp[integrated from pyauto]

--- main.py
+++ main.py
@@ -8,31 +8,49 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import pyautogui
 import webbrowser 
+import subprocess
+
+
+#TODO: 
+# 1.завернуть вся функ в отдельный файл
+# 2.добавить векторазед метод определения комманды
+# 3. завернуть комманды в дикшенери как в https://www.youtube.com/watch?v=MXdsPKZyZ48&t=1205s 
+# 4. не забывать коммиты и пуш в репу!
+# 5. подумать что еще можно заскриптовать полезное #
+
 
 rec = sr.Recognizer()
 rec.pause_threshold = 0.5
-rec.energy_threshold = 0
-rec.phrase_threshold = 0.1
-# rec.dynamic_energy_ratio = 0.1
+rec.energy_threshold = 300
+rec.phrase_threshold = 0.25
+rec.dynamic_energy_ratio = 3
 url_playlist = "https://www.youtube.com/playlist?list=PLWYzwmsMUvFjNHF5LfshndZ5XX8b8l6GM"
 url_tw_btc = "https://ru.tradingview.com/chart/WBPF9YtH/?symbol=BINANCE%3ABTCUSDT"
 
 #default browser
 def task_playMusic_defaultBrowser():
-    webbrowser.open_new(url_playlist)
-    sleep(2)
+    webbrowser.open(url_playlist)
+    sleep(1.2)
     pyautogui.click(x=744, y=344, clicks=1, interval=1, button='left')
-    sleep(0.05)
+    sleep(1.2)
     pyautogui.click(x=1805, y=13, clicks=1, interval=1, button='left')
     
 def task_chart__defaultBrowser():
-    webbrowser.open_new(url_tw_btc)
+    webbrowser.open(url_tw_btc)
     
 def hide_browser_window(): 
     pyautogui.click(x=1805, y=13, clicks=1, interval=1, button='left')
 
 def alt_tab():    
     pyautogui.hotkey('alt','tab')
+
+def launch_rdp():
+    subprocess.Popen(r'C:\WINDOWS\system32\mstsc.exe')
+    sleep(0.2)
+    pyautogui.click(1033,428)
+
+def stop_bot():
+    quit()
 
 #selenium
 def task_playMusic():
@@ -82,8 +100,12 @@ def main():
         task_chart__defaultBrowser()
     elif "скрыть" in words_list or "вкладку" in words_list:
         hide_browser_window()
-    elif "tab" in words_list or "табуляция" in words_list:
+    elif "tab" in words_list or "табуляция" in words_list or 'овуляция' in words_list:
         alt_tab()
+    elif "удалённый" in words_list and "комп" in words_list or 'рдп' in words_list:
+        launch_rdp()
+    elif "выключись" in words_list and "бот" in words_list or 'выключить' in words_list and 'бота' in words_list:
+        stop_bot()
     else:
         print("NOT FOUND")
     print("Command successfuly done")
@@ -94,6 +116,6 @@ if __name__ == "__main__":
             main()
         except Exception as e:
             print(e)
-            sleep(5)
+            sleep(0.1)
             pass
         
